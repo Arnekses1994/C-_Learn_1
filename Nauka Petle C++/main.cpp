68674d011c5a0903945a6d52ab369e6e22b0f852
Artur <arturwojciechturek@wp.pl>
2022-11-29 22:00:40 +0100

    +

---
+++ Nauka Petle C++/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    for (int i=15; i>=0; i--)
+    {
+        Sleep(10);
+        system ("cls");
+        cout << i <<endl;
+    }
+    cout <<"JEBUD";
+
+     return 0;
+}
+
+
